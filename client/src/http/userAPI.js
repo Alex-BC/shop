27085646cf0068a0854518e9e0233a1056b107a3
Alexandr Chepak <djalexbc@gmail.com>
2022-01-12 13:25:25 +0200

    made client

---
+++ client/src/http/userAPI.js
@@ -0,0 +1,28 @@
+import { $authHost, $host } from "./index";
+// import jwt_decode from "jwt-decode";
+
+export const registration = async (email, password) => {
+  const response = await $host.post("api/user/registration", {
+    email,
+    password,
+    role: "ADMIN",
+  });
+  //   localStorage.setItem("token", data.token);
+  //   return jwt_decode(data.token);
+  return response;
+};
+
+export const login = async (email, password) => {
+  const response = await $host.post("api/user/login", { email, password });
+  //   localStorage.setItem("token", data.token);
+  //   return jwt_decode(data.token);
+  return response;
+};
+
+export const check = async () => {
+  const response = await $host.post("api/auth/registration");
+  //   const response = await $authHost.get("api/auth/auth");
+  //   localStorage.setItem("token", data.token);
+  //   return jwt_decode(data.token);
+  return response;
+};
